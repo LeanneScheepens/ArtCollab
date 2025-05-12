a5853fe614c5f71f6a6dcccfb2411754b8bcdcc2
LeanneScheepens <552870@student.fontys.nl>
2025-05-12 15:19:55 +0200

    Created Artist

---
+++ Logic/Models/Artist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArtCollab.Models;
+
+namespace Logic.Models
+{
+    public class Artist : User
+    {
+        public Role Role { get; set; }
+
+        public Artist(int id, string name, string email, string password, string profilePicture, string biography, int role) : base(id, name, email, password, profilePicture, biography)
+        {
+            Role = (Role)role;
+        }
+    }
+}
---
+++ Logic/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Models
+{
+    public enum Role
+    {
+        Artist = 1,
+        Admin = 2,
+    }
+}
