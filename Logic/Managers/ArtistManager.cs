20497c2110eb6cb306fd1385bed7dace7012e292
LeanneScheepens <552870@student.fontys.nl>
2025-05-13 15:08:24 +0200

    Create ArtistManager.cs

---
+++ Logic/Managers/ArtistManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.Models;
+using Logic.Interfaces;
+
+namespace Logic.Managers
+{
+    public class ArtistManager
+    {
+        private readonly IArtistRepository _artistRepository;
+
+        public ArtistManager(IArtistRepository artistRepository)
+        {
+            _artistRepository = artistRepository;
+        }
+
+        public List<Artist> GetArtists()
+        {
+            return _artistRepository.GetArtists();
+        }
+
+        public void DeleteArtist(List<int> ids)
+        {
+            foreach (int id in ids)
+            {
+                _artistRepository.DeleteArtist(id);
+            }
+        }
+
+        public void CreateArtist(Artist artist)
+        {
+            _artistRepository.CreateArtist(artist);
+            Console.WriteLine("Manager:" + artist.Name);
+        }
+    }
+}
+
+
