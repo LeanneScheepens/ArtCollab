34048a91e6379d61c5206f335aa24bfbe237d8b2
LeanneScheepens <552870@student.fontys.nl>
2025-05-15 10:00:19 +0200

    Update Artwork

--- Logic/Models/Artwork.cs
+++ Logic/Models/Artwork.cs
@@ -4,26 +4,31 @@ namespace ArtCollab.Models
     public class Artwork
     {
 
-        private static int seed = 1;
+        //private static int seed = 1;
         private int id;
         private string title;
         private string description;
-        private string url;
         private string owner;
+        private DateTime uploadDate;
+        private string imageUrl;
 
-        public int Id { get; }
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Owner { get; set; }
+        public DateTime UploadDate { get; set; }
+        public string ImageUrl { get; set; }
 
-        public Artwork (int id, string title, string description, string url, string owner)
+        public List<int> UserIds { get; set; } = new();
+
+        public Artwork (int id, string title, string description, string owner, DateTime uploadDate, string imageUrl)
         {
-            this.id = seed;
-            seed++;
-            this.title = title;
-            this.description = description;
-            this.url = url;
-            this.owner = owner;
+            Id = id;
+            Title = title;
+            Description = description;
+            Owner = owner;
+            UploadDate = uploadDate;
+            ImageUrl = imageUrl;
         }
 
     }
