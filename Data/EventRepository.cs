3854c369f37758120a4cf481cefc1a38e90df184
LeanneScheepens <552870@student.fontys.nl>
2025-05-26 13:55:38 +0200

    Clean up

--- Data/EventRepository.cs
+++ Data/EventRepository.cs
@@ -23,31 +23,43 @@ namespace Data
         {
             using var connection = new SqlConnection(_connectionString);
             connection.Open();
-            var cmd = new SqlCommand(@"INSERT INTO Events (Title, Description, Owner) 
-                                       VALUES (@Title, @Description, @Owner); 
-                                       SELECT SCOPE_IDENTITY();", connection);
+
+            var cmd = new SqlCommand(@"
+                INSERT INTO Event (Title, StartDate, EndDate, Description, Owner) 
+                VALUES (@Title, @StartDate, @EndDate, @Description, @Owner); 
+                SELECT SCOPE_IDENTITY();", connection);
+
             cmd.Parameters.AddWithValue("@Title", evt.Title);
+            cmd.Parameters.AddWithValue("@StartDate", evt.StartDate);
+            cmd.Parameters.AddWithValue("@EndDate", evt.EndDate);
             cmd.Parameters.AddWithValue("@Description", evt.Description);
             cmd.Parameters.AddWithValue("@Owner", evt.Owner);
+
             evt.Id = Convert.ToInt32(cmd.ExecuteScalar());
         }
 
         public List<Event> GetAllEvents()
         {
             var events = new List<Event>();
+
             using var connection = new SqlConnection(_connectionString);
             connection.Open();
-            var cmd = new SqlCommand("SELECT Id, Title, Description, Owner FROM Events", connection);
+
+            var cmd = new SqlCommand("SELECT Id, Title, StartDate, EndDate, Description, Owner FROM Event", connection);
             using var reader = cmd.ExecuteReader();
+
             while (reader.Read())
             {
                 events.Add(new Event(
-                    reader.GetInt32(0),
-                    reader.GetString(1),
-                    reader.GetString(2),
-                    reader.GetString(3)
+                    reader.GetInt32(0),      // Id
+                    reader.GetString(1),     // Title
+                    reader.GetDateTime(2),   // StartDate
+                    reader.GetDateTime(3),   // EndDate
+                    reader.GetString(4),     // Description
+                    reader.GetString(5)      // Owner
                 ));
             }
+
             return events;
         }
 
@@ -55,18 +67,23 @@ namespace Data
         {
             using var connection = new SqlConnection(_connectionString);
             connection.Open();
-            var cmd = new SqlCommand("SELECT Id, Title, Description, Owner FROM Events WHERE Id = @Id", connection);
+
+            var cmd = new SqlCommand("SELECT Id, Title, StartDate, EndDate, Description, Owner FROM Event WHERE Id = @Id", connection);
             cmd.Parameters.AddWithValue("@Id", id);
+
             using var reader = cmd.ExecuteReader();
             if (reader.Read())
             {
                 return new Event(
                     reader.GetInt32(0),
                     reader.GetString(1),
-                    reader.GetString(2),
-                    reader.GetString(3)
+                    reader.GetDateTime(2),
+                    reader.GetDateTime(3),
+                    reader.GetString(4),
+                    reader.GetString(5)
                 );
             }
+
             return null;
         }
 
@@ -74,6 +91,7 @@ namespace Data
         {
             using var connection = new SqlConnection(_connectionString);
             connection.Open();
+
             foreach (var artworkId in artworkIds)
             {
                 var cmd = new SqlCommand("INSERT INTO EventArtworks (EventId, ArtworkId) VALUES (@EventId, @ArtworkId)", connection);
@@ -84,4 +102,3 @@ namespace Data
         }
     }
 }
-
