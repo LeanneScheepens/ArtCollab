20c889ca5d3a66bfefe0330e4fbbaf65b7238e0d
LeanneScheepens <552870@student.fontys.nl>
2025-06-03 14:53:18 +0200

    interface Collection

---
+++ Logic/Interfaces/ICollectionRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.Models;
+using ArtCollab.Models;
+
+
+namespace Logic.Interfaces
+{
+    public interface ICollectionRepository
+    {
+        void CreateCollection(Collection collection);
+        List<Collection> GetCollectionsByOwner(string owner);
+        void AddArtworksToCollection(int collectionId, List<int> artworkIds);
+        Collection GetCollectionById(int id);
+        void AddArtworkToCollectionIfNotExists(int collectionId, int artworkId);
+        void DeleteCollection(int collectionId, string owner);
+
+    }
+}
