73a951673d52dcd81b48e9f9eec15c3c72516e95
LeanneScheepens <552870@student.fontys.nl>
2025-06-03 14:52:41 +0200

    Collection manager

---
+++ Logic/Managers/CollectionManager.cs
@@ -0,0 +1,31 @@
+﻿using Logic.Models;
+using Logic.Interfaces;
+
+namespace Logic.Managers
+{
+    public class CollectionManager
+    {
+        private readonly ICollectionRepository _repo;
+
+        public CollectionManager(ICollectionRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public void CreateCollection(Collection collection) => _repo.CreateCollection(collection);
+
+        public List<Collection> GetCollectionsByOwner(string owner) => _repo.GetCollectionsByOwner(owner);
+
+        public void AddArtworkToCollection(int collectionId, int artworkId)
+        {
+            _repo.AddArtworkToCollectionIfNotExists(collectionId, artworkId);
+        }
+        public void DeleteCollection(int collectionId, string owner)
+        {
+            _repo.DeleteCollection(collectionId, owner);
+        }
+
+
+        public Collection GetCollectionById(int id) => _repo.GetCollectionById(id);
+    }
+}
