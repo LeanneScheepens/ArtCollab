70574e2119bb5326412daf32b2ab6f4728cceffc
LeanneScheepens <552870@student.fontys.nl>
2025-06-04 11:25:41 +0200

    Create ICommentRepository.cs

---
+++ Logic/Interfaces/ICommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.Models;
+
+namespace Logic.Interfaces
+{
+    public interface ICommentRepository
+    {
+        void CreateComment(Comment comment);
+        void DeleteComment(int id);
+        void UpdateComment(Comment comment);
+        List<Comment> GetComments(int artworkId);
+    }
+}
