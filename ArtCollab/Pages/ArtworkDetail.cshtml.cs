ad521b0fdd763efe8c68cc38cfcb8e220dd31775
LeanneScheepens <552870@student.fontys.nl>
2025-06-12 22:40:10 +0200

    Update ArtworkDetail for adding comments

--- ArtCollab/Pages/ArtworkDetail.cshtml.cs
+++ ArtCollab/Pages/ArtworkDetail.cshtml.cs
@@ -2,6 +2,7 @@ using Logic.Models;
 using Logic.Managers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ArtCollab.Services;
 
 namespace ArtCollab.Pages
 {
@@ -9,11 +10,13 @@ namespace ArtCollab.Pages
     {
         private readonly ArtworkManager _artworkManager;
         private readonly CollectionManager _collectionManager;
+        private readonly CommentManager _commentManager;
 
-        public ArtworkDetailModel(ArtworkManager artworkManager, CollectionManager collectionManager)
+        public ArtworkDetailModel(ArtworkManager artworkManager, CollectionManager collectionManager, CommentManager commentManager)
         {
             _artworkManager = artworkManager;
             _collectionManager = collectionManager;
+            _commentManager = commentManager;
         }
 
         [TempData]
@@ -25,6 +28,20 @@ namespace ArtCollab.Pages
         public int SelectedCollectionId { get; set; }
 
         public List<Logic.Models.Collection> UserCollections { get; set; } = new();
+        public List<Comment> Comments { get; set; } = new();
+
+        [BindProperty]
+        public string NewCommentContent { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int? EditCommentId { get; set; }
+
+        [BindProperty]
+        public int CommentId { get; set; }
+
+        [BindProperty]
+        public string NewContent { get; set; }
+
 
         public IActionResult OnGet(int id)
         {
@@ -33,9 +50,56 @@ namespace ArtCollab.Pages
 
             var owner = User.Identity.Name;
             UserCollections = _collectionManager.GetCollectionsByOwner(owner);
+            Comments = _commentManager.GetCommentsByArtworkId(id);
 
             return Page();
         }
+        public IActionResult OnPostAddComment(int id)
+        {
+            if (string.IsNullOrWhiteSpace(NewCommentContent))
+                return RedirectToPage(new { id });
+
+            var author = User.Identity?.Name ?? "Anonymous";
+            var comment = new Comment(0, id, NewCommentContent, author, DateTime.Now);
+
+            _commentManager.AddComment(comment);
+            return RedirectToPage(new { id });
+        }
+        public IActionResult OnPostDeleteComment(int id, int commentId)
+        {
+            var user = User.Identity?.Name;
+            var isAdmin = User.IsInRole("Admin");
+
+            var comment = _commentManager.GetCommentsByArtworkId(id)
+                                          .FirstOrDefault(c => c.Id == commentId);
+
+            if (comment == null)
+                return NotFound();
+
+            var isOwner = comment.Author == user;
+            if (!isOwner && !isAdmin)
+                return Forbid();
+
+            _commentManager.DeleteComment(commentId);
+            return RedirectToPage(new { id });
+        }
+        public IActionResult OnPostEditComment(int id, int commentId, string newContent)
+        {
+            var user = User.Identity?.Name;
+            if (string.IsNullOrWhiteSpace(newContent))
+                return RedirectToPage(new { id });
+
+            var comment = _commentManager.GetCommentsByArtworkId(id).FirstOrDefault(c => c.Id == commentId);
+            if (comment == null || comment.Author != user)
+                return Forbid();
+
+            comment.Content = newContent;
+            comment.UploadDate = DateTime.Now;
+
+            _commentManager.UpdateComment(comment);
+            return RedirectToPage(new { id });
+        }
+
 
         public IActionResult OnPost(int id)
         {
