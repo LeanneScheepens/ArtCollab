c40ba39b531a1c6617238c250e84974220755017
LeanneScheepens <552870@student.fontys.nl>
2025-06-13 12:44:03 +0200

    Create EditArtworkViewModel.cs

---
+++ Logic/ViewModels/EditArtworkViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Logic.ViewModels
+{
+    public class EditArtworkViewModel
+    {
+        public int ArtworkId { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Image URL is required")]
+        public string ImageUrl { get; set; }
+
+        [Required(ErrorMessage = "Owner is required")]
+        public string Owner { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Upload Date is required")]
+        [DataType(DataType.Date)]
+        public DateTime UploadDate { get; set; }
+    }
+}
